6e574d1564ba3e20f3e1aa038582c6ce5b5080fb
JackBai <xingjianbai0914@sina.com>
2020-11-27 01:08:51 +0000

    7.0

--- latest_version/run.sh
+++ latest_version/run.sh
@@ -1,12 +1,2 @@
 # ./brute 48 >ans.txt <$1
-time ./combine 48 >ans.txt <$1
-
-time ./combine 40 >ans.txt <$1
-
 time ./combine 36 >ans.txt <$1
-
-time ./combine 30 >ans.txt <$1
-
-time ./combine 16 >ans.txt <$1
-
-time ./combine 8 >ans.txt <$1
