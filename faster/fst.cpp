d4b098c3447e8a96e2c36120162e27ac7130f6cd
JackBai <xingjianbai0914@sina.com>
2020-11-30 15:14:55 +0000

    10

--- faster/fst.cpp
+++ faster/fst.cpp
@@ -122,16 +122,22 @@ int main() {
 		chkmax(n, u), chkmax(n, v);
 	}
 	rep(i, 1, n) solve(i);
-	double bg = 0;
-	rep(i, 1, n) chkmax(bg, bet[i]);
-	putchar('[');
-    cerr << fixed << setprecision(10);
 
-	// cerr << bg << endl;
-	// rep (i, 1, n)
-	// 	if (i == 362)
-	// 		cerr << bet[i] << " " << bg << " " << bet[i] / bg << " " << bet[i] / bg + eps << endl;
-	rep(i, 1, n) if(exi[i]) printf("(%d,%.2f)", i-1, bet[i] / bg + eps), cout << ",]"[i == n];
-	// rep(i, 1, n) if(exi[i]) printf("(%d,%.2lf)", i-1, bet[i]), cout << ",]"[i == n];
+
+    double mx = 0;
+    rep(i, 1, n) chkmax(mx, bet[i]);
+
+    cout << fixed << setprecision(2);
+    cout << '[';
+    bool ini = true;
+    rep(i, 1, n) {
+        if (exi[i]) {
+            if (!ini)
+                cout << ',';
+            cout << '(' << i-1 << ',' << bet[i] / bg << ')';
+            ini = false;
+        }
+    }
+    cout << ']';
 	return 0;
 }
