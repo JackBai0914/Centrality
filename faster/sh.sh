6595796e9324e1033ef28286d5db76539e4c4019
JackBai <xingjianbai0914@sina.com>
2020-11-30 15:10:44 +0000

    10

--- faster/sh.sh
+++ faster/sh.sh
@@ -1,9 +1,9 @@
 g++ -std=c++11 combined.cpp -O3 -o combine -lpthread 
-g++ -std=c++11 fst.cpp -O3 -o fst -lpthread -fsanitize=address -ftrapv -Xpreprocessor -fopenmp
+g++ -std=c++11 fst.cpp -O3 -o fst -lpthread -fopenmp
 g++ -std=c++11 gen.cpp -O3 -o gen
 while true;do
 	./gen >inp.in
- 	time ./combine 4 >combine.out <inp.in
+ 	time ./combine 24 >combine.out <inp.in
  	time ./fst >fst.out <inp.in
 
 if diff combine.out fst.out
